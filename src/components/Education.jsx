6584e3887a43863af820d6cb80c281bf419e56b4
Tushar Anand <tusharanand@MacBookAir.mshome.net>
2026-08-20 12:20:21 +0530

    academic details chnaged

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,8 +1,20 @@
 import WaveWords from './WaveWords'
 
 const qualifications = [
-  { period: '2020 - 2022', degree: 'Master of Computer Applications', short: 'MCA' },
-  { period: '2017 - 2020', degree: 'Bachelor of Computer Applications', short: 'BCA' },
+  {
+    period: '2020 - 2022',
+    degree: 'Master of Computer Applications',
+    short: 'MCA',
+    college: 'Marwari College, Ranchi',
+    marks: '85%'
+  },
+  {
+    period: '2017 - 2020',
+    degree: 'Bachelor of Computer Applications',
+    short: 'BCA',
+    college: 'Marwari College, Ranchi',
+    marks: '80%'
+  },
 ]
 
 export default function Education() {
@@ -19,7 +31,9 @@ export default function Education() {
             <span className="qualification-number"><WaveWords>{`0${index + 1}`}</WaveWords></span>
             <p className="qualification-period"><WaveWords>{qualification.period}</WaveWords></p>
             <h3><WaveWords>{qualification.degree}</WaveWords></h3>
+            <p className="qualification-college"><WaveWords>{qualification.college}</WaveWords></p>
             <span className="qualification-short"><WaveWords>{qualification.short}</WaveWords></span>
+            <p className="qualification-marks"><WaveWords>{qualification.marks}</WaveWords></p>
           </article>
         ))}
       </div>
